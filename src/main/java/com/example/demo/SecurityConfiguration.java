9c6c18f12268f247d4df89f945c3d9e0e7cc20ee
b <jnohtr@gmail.com>
2018-07-16 15:39:06 -0400

    first commit

---
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").access("hasAnyAuthority('USER', 'ADMIN')")
+                .antMatchers("/admin").access("hasAuthority('ADMIN')")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .and().httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+        throws Exception {
+
+        PasswordEncoder p = new BCryptPasswordEncoder();
+
+        auth.inMemoryAuthentication().
+//                withUser("dave").password("password").roles("ADMIN"). dMSpeedwagon
+                withUser("MSpeedwagon").password(p.encode("pass")).authorities("ADMIN").
+                and().
+                withUser("user").password(p.encode("pass")).authorities("USER")
+                .
+                and().
+                passwordEncoder(new BCryptPasswordEncoder());
+
+    }
+
+//    @SuppressWarnings("deprecation")
+//    @Bean
+//    public static NoOpPasswordEncoder passwordEncoder() {
+//        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+//    }
+}
